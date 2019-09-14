873c5a0ef287eeb4eeaa4538b95d4f68d85d0fa7
kabeh2 <mattcnk@hotmail.com>
2019-09-14 00:42:25 -0400

    Updates

---
+++ precache-manifest.5a4c5b84dab95d988d9beff54b69863e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3346a40cd28afca5abc4c4fba91cd8c",
+    "url": "/React-Github-User-Card/index.html"
+  },
+  {
+    "revision": "7cb5dca03cf3d887cb13",
+    "url": "/React-Github-User-Card/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "39e43874bae2f3368136",
+    "url": "/React-Github-User-Card/static/css/main.07810dda.chunk.css"
+  },
+  {
+    "revision": "7cb5dca03cf3d887cb13",
+    "url": "/React-Github-User-Card/static/js/2.d5a99cb4.chunk.js"
+  },
+  {
+    "revision": "39e43874bae2f3368136",
+    "url": "/React-Github-User-Card/static/js/main.4a664456.chunk.js"
+  },
+  {
+    "revision": "83d4e9e99b082ef51fd2",
+    "url": "/React-Github-User-Card/static/js/runtime~main.4fde055b.js"
+  }
+]);
